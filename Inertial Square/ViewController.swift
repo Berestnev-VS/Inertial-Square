fd2d4f799f9b47cb5d3fddc16c2d4bbc20a92a2e
Berestnev Vladimir <berestnev.sv@gmail.com>
2023-05-17 10:28:14 +0300

    updated

--- Inertial Square/ViewController.swift
+++ Inertial Square/ViewController.swift
@@ -4,37 +4,55 @@
 //
 //  Created by Владимир on 16.05.2023.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
     
-    var squareView: UIView!
+    var squareView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.systemBlue
+        view.layer.cornerRadius = 12
+        view.layer.cornerCurve = .continuous
+        return view
+    }()
+    
+    var animator: UIDynamicAnimator!
+    
+    var collision: UICollisionBehavior!
+    
+    var gravity : UIGravityBehavior!
+    
+    var snap: UISnapBehavior!
+
+    var dynamicItemBehavior: UIDynamicItemBehavior!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        squareView = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        squareView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
         squareView.center = self.view.center
-        squareView.backgroundColor = UIColor.systemBlue
-        squareView.layer.cornerRadius = 12
-        squareView.layer.cornerCurve = .continuous
         self.view.addSubview(squareView)
         
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.handleTap(_:)))
-        self.view.addGestureRecognizer(tapGesture)
+        animator = UIDynamicAnimator(referenceView: view)
+        
+        collision = UICollisionBehavior(items: [squareView])
+        collision.translatesReferenceBoundsIntoBoundary = true
+        animator.addBehavior(collision)
+        
     }
     
-    @objc func handleTap(_ gesture: UITapGestureRecognizer) {
-        let touchPoint = gesture.location(in: self.view)
-        
-        UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseInOut, animations: {
-            self.squareView.center = touchPoint
-            self.squareView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi / 4))
-        }) { (finished) in
-            UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseInOut, animations: {
-                self.squareView.transform = CGAffineTransform.identity
-            })
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if (snap != nil) {
+            animator.removeBehavior(snap)
         }
+        
+        let touch = touches.first
+        snap = UISnapBehavior(item: squareView, snapTo: touch!.location(in: view))
+        animator.addBehavior(snap)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        
     }
 }
